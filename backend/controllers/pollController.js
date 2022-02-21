b5e279f0ffab19f932f5c3c52d36c69ab94e833a
Akash Kumar Prasad <akash22gupta33@gmail.com>
2022-02-21 23:46:12 +0530

    update

--- backend/controllers/pollController.js
+++ backend/controllers/pollController.js
@@ -23,3 +23,75 @@ exports.getAllCities = async (req,res)=>{
     return res.status(500).json({ message: e.message, status: "Failed" });
     }
 }
+exports.sortCity = async(req,res,next) =>{
+    try{
+    const electionpoll = await Electionpoll.find({placeType:"City"});
+
+    res.status(201).json({
+        success:true,
+        electionpoll
+    })
+}catch(e){
+    return res.status(500).json({ message: e.message, status: "Failed" });
+}
+}
+exports.sortTown = async(req,res,next) =>{
+    try{
+    const electionpoll = await Electionpoll.find({placeType:"Town"});
+
+    res.status(201).json({
+        success:true,
+        electionpoll
+    })
+}catch(e){
+    return res.status(500).json({ message: e.message, status: "Failed" });
+}
+}
+exports.sortVillage = async(req,res,next) =>{
+    try{
+    const electionpoll = await Electionpoll.find({placeType:"Village"});
+
+    res.status(201).json({
+        success:true,
+        electionpoll
+    })
+}catch(e){
+    return res.status(500).json({ message: e.message, status: "Failed" });
+}
+}
+exports.sortMetroCity = async(req,res,next) =>{
+    try{
+    const electionpoll = await Electionpoll.find({placeType:"Metro City"});
+
+    res.status(201).json({
+        success:true,
+        electionpoll
+    })
+}catch(e){
+    return res.status(500).json({ message: e.message, status: "Failed" });
+}
+}
+exports.highPolling = async(req,res,next) =>{
+    try{
+    const electionpoll = await Electionpoll.find().limit();
+
+    res.status(201).json({
+        success:true,
+        electionpoll
+    })
+}catch(e){
+    return res.status(500).json({ message: e.message, status: "Failed" });
+}
+}
+exports.lowPolling = async(req,res,next) =>{
+    try{
+    const electionpoll = await Electionpoll.find();
+
+    res.status(201).json({
+        success:true,
+        electionpoll
+    })
+}catch(e){
+    return res.status(500).json({ message: e.message, status: "Failed" });
+}
+}
--- backend/routers/pollRoute.js
+++ backend/routers/pollRoute.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getAllCities,createElectionPoll } = require("../controllers/pollController");
+const { getAllCities,createElectionPoll, sortCity, sortVillage,sortTown ,sortMetroCity} = require("../controllers/pollController");
 const router = express.Router();
 
 router.route("/name").get(getAllCities);  //get alldata
 router.route("/name/election").post(createElectionPoll); // 
+router.route("/name/filter/city").get(sortCity); //
+router.route("/name/filter/town").get(sortTown); // 
+router.route("/name/filter/village").get(sortVillage); // 
+router.route("/name/filter/Metrocity").get(sortMetroCity); // 
+
 
 
 module.exports = router
